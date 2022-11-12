feb1e2619df2f9782298fb7c2e1f85b5c70cf78b
Edgar Rokian <edrokyan@microsoft.com>
2022-11-12 20:58:05 +0100

    1605. Find Valid Matrix Given Row and Column Sums.

--- medium/cpp/1605.cpp
+++ medium/cpp/1605.cpp
@@ -5,48 +5,20 @@ class Solution
 public:
     std::vector<std::vector<int>> restoreMatrix(std::vector<int>& rowSum, std::vector<int>& colSum)
     {
-        const std::size_t n = std::size(rowSum);
-        const std::size_t m = std::size(colSum);
+        const auto n = std::size(rowSum);
+        const auto m = std::size(colSum);
         
         std::vector<std::vector<int>> solution(n, std::vector<int>(m));
         
-        while (true)
+        for (std::size_t i = 0; i < n; i++)
         {
-            int row_largest = 0;
-            
-            for (std::size_t row = 1; row < n; row++)
+            for (std::size_t j = 0; j < m; j++)
             {
-                if (rowSum[row] > rowSum[row_largest])
-                {
-                    row_largest = row;
-                }
+                const auto val = std::min(rowSum[i], colSum[j]);
+                solution[i][j] = val;
+                rowSum[i] -= val;
+                colSum[j] -= val;
             }
-            
-            if (rowSum[row_largest] == 0)
-            {
-                break;
-            }
-            
-            int col_largest = 0;
-            
-            for (std::size_t col = 1; col < m; col++)
-            {
-                if (colSum[col] > colSum[col_largest])
-                {
-                    col_largest = col;
-                }
-            }
-            
-            if (colSum[col_largest] == 0)
-            {
-                break;
-            }
-            
-            const auto am = std::min(rowSum[row_largest], colSum[col_largest]);
-            
-            solution[row_largest][col_largest] += am;
-            rowSum[row_largest] -= am;
-            colSum[col_largest] -= am;
         }
         
         return solution;

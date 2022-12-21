8fa2fbfc638d4ec95c9ea6067033c7f12cb01450
Edgar Rokian <edrokyan@microsoft.com>
2022-12-21 22:29:35 +0100

    22. Generate Parentheses.

---
+++ medium/cpp/22.cpp
@@ -0,0 +1,40 @@
+// https://leetcode.com/problems/generate-parentheses/description/
+
+class Solution
+{
+public:     
+    std::vector<std::string> generateParenthesis(int n)
+    {
+        seq.resize(2 * n);
+
+        generate(0, 0, n);
+
+        return ans;
+    }
+
+private:
+   void generate(int opened, int closed, int n)
+   {
+        if (opened == n && closed == n)
+        {
+            ans.push_back(seq);
+            return;
+        }
+        
+        if (opened < n)
+        {
+            seq[opened + closed] = '(';
+            generate(opened + 1, closed, n);
+        }
+        
+        if (closed < opened)
+        {
+            seq[opened + closed] = ')';
+            generate(opened, closed + 1, n);
+        }
+    }
+
+private:
+    std::string seq;
+    std::vector<std::string> ans;
+};

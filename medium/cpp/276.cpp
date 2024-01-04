587af6e7383d1bf499e48b450f1ab79d76cb4932
Edgar Rokian <edrokyan@microsoft.com>
2024-01-04 13:01:40 +0100

    276. Paint Fence.

--- medium/cpp/276.cpp
+++ medium/cpp/276.cpp
@@ -1,5 +1,6 @@
 // https://leetcode.com/problems/paint-fence/description/
 
+
 class Solution
 {
 public:
@@ -10,29 +11,20 @@ public:
             return k;
         }
 
-        std::vector<std::vector<int>> dp(n, std::vector<int>(k));
+        std::vector<int> dp(n);
 
         for (auto i = 0; i < k; i++)
         {
-            dp[0][i] = 1;
-            dp[1][i] = k;
+            dp[0] = k;
+            dp[1] = k * k;
         }
 
         for (auto i = 2; i < n; i++)
         {
-            for (auto j = 0; j < k; j++)
-            {
-                dp[i][j] += (k - 1) * (dp[i - 1][j] + dp[i - 2][j]);
-            }
+            dp[i] += (k - 1) * (dp[i - 1] + dp[i - 2]);
+
         }
 
-        auto res = 0;
-
-        for (auto i = 0; i < k; i++)
-        {
-            res += dp.back()[i];
-        }
-
-        return res;
+        return dp.back();
     }
 };

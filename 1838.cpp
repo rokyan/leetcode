fe0aa76d4de770873a7a0eadcb8a216896ed7312
Edgar Rokian <edrokyan@microsoft.com>
2024-01-31 22:29:17 +0100

    1838. Frequency of the Most Frequent Element.

--- 1838.cpp
+++ 1838.cpp
@@ -6,29 +6,20 @@ public:
     int maxFrequency(std::vector<int>& nums, int k)
     {
         std::sort(std::begin(nums), std::end(nums));
-        
+
         const auto n = std::size(nums);
 
-        std::vector<long long> pref(n);
-
-        for (auto i = 0; i < n; i++)
-        {
-            pref[i] = nums[i];
-
-            if (i > 0)
-            {
-                pref[i] += pref[i - 1];
-            }
-        }
-
         auto res = 0;
+        auto s = 0LL;
 
         for (auto l = 0, r = 0; r < n; r++)
         {
-            auto cur = 1LL * (r - l + 1) * nums[r] - (pref[r] - (l > 0 ? pref[l - 1] : 0));
+            s += nums[r];
+            auto cur = 1LL * (r - l + 1) * nums[r] - s;
 
             while (cur > k)
             {
+                s -= nums[l];
                 cur -= (nums[r] - nums[l++]);
             }
 

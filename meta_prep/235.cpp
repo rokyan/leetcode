f86e275cd76dede5341c83f7efcaffe5a8441f74
Edgar Rokian <edrokyan@microsoft.com>
2025-03-06 22:24:35 +0100

    Add 235.

---
+++ meta_prep/235.cpp
@@ -0,0 +1,34 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+
+class Solution
+{
+public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q)
+    {
+        while (root != nullptr)
+        {
+            if (std::max(p->val, q->val) < root->val)
+            {
+                root = root->left;
+            }
+            else if (std::min(p->val, q->val) > root->val)
+            {
+                root = root->right;
+            }
+            else
+            {
+                return root;
+            }
+        }
+
+        return root;
+    }
+};

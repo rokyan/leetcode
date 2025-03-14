8594feeab07cabcd520caa3bb7cdd9bd1b9b7896
Edgar Rokian <edrokyan@microsoft.com>
2025-03-14 15:59:20 +0100

    Add 7.

---
+++ meta_prep/7.cpp
@@ -0,0 +1,38 @@
+class Solution
+{
+private:
+    static constexpr auto MAX = (1LL << 31) - 1;
+
+public:
+    int reverse(int x)
+    {
+        auto cp = 1LL * x;
+
+        const auto negative = (cp < 0);
+
+        if (negative)
+        {
+            cp = -cp;
+        }
+
+        auto res = 0LL;
+
+        while (cp > 0)
+        {
+            res = 10 * res + cp % 10;
+            cp /= 10;
+
+            if (res > MAX)
+            {
+                return 0;
+            }
+        }
+
+        if (negative)
+        {
+            res = -res;
+        }
+
+        return static_cast<int>(res);
+    }
+};

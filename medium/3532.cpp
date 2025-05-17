628d35e98af724f3785cb013c7756c0e0a0bae83
Edgar Rokian <edrokyan@microsoft.com>
2025-05-17 17:31:50 +0200

    3532. Path Existence Queries in a Graph I

--- medium/3532.cpp
+++ medium/3532.cpp
@@ -9,29 +9,11 @@ public:
         int maxDiff,
         const std::vector<std::vector<int>>& queries)
     {
-        std::vector<std::pair<int, int>> numsWithPos(n);
-
-        for (auto i = 0; i < n; i++)
-        {
-            numsWithPos[i] = {nums[i], i};
-        }
-
-        std::ranges::sort(numsWithPos);
-
-        std::vector<int> inv(n);
-
-        auto i = 0;
-
-        for (const auto [_, pos] : numsWithPos)
-        {
-            inv[pos] = i++;
-        }
-
         std::vector<int> longest(n, 1);
 
         for (auto i = 1; i < n; i++)
         {
-            if (numsWithPos[i].first - numsWithPos[i - 1].first <= maxDiff)
+            if (nums[i] - nums[i - 1] <= maxDiff)
             {
                 longest[i] += longest[i - 1];
             }
@@ -42,8 +24,8 @@ public:
 
         for (auto q = 0; q < qc; q++)
         {
-            auto p1 = inv[queries[q][0]];
-            auto p2 = inv[queries[q][1]];
+            auto p1 = queries[q][0];
+            auto p2 = queries[q][1];
 
             if (p1 > p2)
             {

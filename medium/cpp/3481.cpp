e1037d223978a8866a9d88e47a5a8a75ae28beb6
Edgar Rokian <edrokyan@microsoft.com>
2025-06-03 20:54:36 +0200

    3481. Apply Substitutions.

---
+++ medium/cpp/3481.cpp
@@ -0,0 +1,65 @@
+// https://leetcode.com/problems/apply-substitutions/description/
+
+class Solution
+{
+public:
+    std::string applySubstitutions(
+        const std::vector<std::vector<std::string>>& replacements,
+        const std::string& text)
+    {
+        return solve(text, replacements);
+    }
+
+private:
+    std::string solve(
+        std::string_view sw,
+        const std::vector<std::vector<std::string>>& replacements)
+    {
+        std::string res;
+        res.reserve(std::size(sw));
+
+        std::cout << sw << std::endl;
+
+        for (auto i = 0; i < std::size(sw); i++)
+        {
+            if (sw[i] == '%')
+            {
+                auto j = i + 1;
+
+                while (sw[j] != '%')
+                {
+                    j++;
+                }
+
+                const auto next = sw.substr(i + 1, j - 1 - (i + 1) + 1);
+                auto found = false;
+
+                i = j;
+
+                for (const auto& e : replacements)
+                {
+                    const auto& target = e[0];
+                    const auto& r = e[1];
+
+                    if (next == target)
+                    {
+                        found = true;
+                        res += solve(r, replacements);
+                        break;
+                    }
+                }
+
+                if (!found)
+                {
+                    res += solve(next, replacements);
+                }
+            }
+            else
+            {
+                res.push_back(sw[i]);
+            }
+        }
+
+        return res;
+    }
+};

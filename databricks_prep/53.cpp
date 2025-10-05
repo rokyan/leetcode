7589e34f5a9377ef72253c44ca7640834b97fa5c
Edgar Rokian <edrokyan@microsoft.com>
2025-10-05 15:18:20 +0200

    53. Maximum Subarray.

---
+++ databricks_prep/53.cpp
@@ -0,0 +1,18 @@
+class Solution
+{
+public:
+    int maxSubArray(std::vector<int>& nums)
+    {
+        auto sum = 0;
+        auto ans = std::numeric_limits<int>::min();
+        
+        for (auto e : nums)
+        {
+            const auto subSum = std::max(e, sum + e);
+            ans = std::max(ans, subSum);
+            sum = subSum;
+        }
+        
+        return ans;
+    }
+};

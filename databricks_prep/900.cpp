d6fca3da51116fdbbf4f7908fbdcdc1c257b1acb
Edgar Rokian <edrokyan@microsoft.com>
2025-10-09 16:10:12 +0200

    900. RLE Iterator.

---
+++ databricks_prep/900.cpp
@@ -0,0 +1,39 @@
+class RLEIterator
+{
+public:
+    RLEIterator(std::vector<int> encoding)
+        : encoding{std::move(encoding)}
+        , idx{0}
+    {        
+    }
+    
+    int next(int n) noexcept
+    {
+        if (idx == std::size(encoding))
+        {
+            return -1;
+        }
+
+        if (n <= encoding[idx])
+        {
+            encoding[idx] -= n;
+            return encoding[idx + 1];
+        }
+        else
+        {
+            n -= encoding[idx];
+            idx += 2;
+            return next(n);
+        }
+    }
+
+private:
+    std::vector<int> encoding;
+    std::size_t idx;
+};
+
+/**
+ * Your RLEIterator object will be instantiated and called as such:
+ * RLEIterator* obj = new RLEIterator(encoding);
+ * int param_1 = obj->next(n);
+ */
